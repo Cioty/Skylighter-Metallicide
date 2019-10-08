47373d8e1a3f4a7af789a14167752aab3c2e5acc
wernut <43165818+wernut@users.noreply.github.com>
2019-10-08 19:20:13 +1100

    Added prefab for updated mech with movement controller. (Located @ Assets/Prefabs/Player_v2.prefab)

---
+++ Project_Prototype/Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    [Header("Objects")]
+    public GameObject playerObject;
+    public Transform mechHullTransform;
+    public Transform mechHipTransform;
+    public Transform mechCoreTransform;
+    private Quaternion defaultHipRotation;
+
+    [Header("Camera Properties")]
+    public float sensitivity = 2.5f;
+    public float smoothing = 2.0f;
+    public float minX = -360, maxX = 360;
+    public float minY = -60, maxY = 80;
+    private Vector2 mouseLook, smoothV;
+
+    private void Start()
+    {
+        // Setting up the default hip transform. (Current static, will implement direction on move soon).
+        this.defaultHipRotation = mechHipTransform.rotation;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Getting the mouse delta.
+        var mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+        mouseDelta = Vector2.Scale(mouseDelta, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+
+        // Getting the interpolated result between the two float values.
+        smoothV.x = Mathf.Lerp(smoothV.x, mouseDelta.x, 1f / smoothing);
+        smoothV.y = Mathf.Lerp(smoothV.y, mouseDelta.y, 1f / smoothing);
+
+        // Incrementally adding to the camera look.
+        mouseLook += smoothV;
+
+        // Locking the mouse Y.
+        mouseLook.y = Mathf.Clamp(mouseLook.y, minY, maxY);
+
+        // Restraining the hip transform to prevent rotation.
+        //this.mechHipTransform.rotation = defaultHipRotation;
+
+        // Applying rotation to the neck transform, and correcting the angle.
+        mechCoreTransform.localRotation = (Quaternion.AngleAxis(-mouseLook.y, Vector3.right));
+
+        // Applying rotation to the player transform.
+        playerObject.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, playerObject.transform.up);
+    }
+}
---
+++ Project_Prototype/Assets/Scripts/MechController.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MechController : MonoBehaviour
+{
+    [Header("References")]
+    public GameObject playerObject;
+    private CharacterController controller;
+
+    [Header("Movement")]
+    public float speed = 5.0f;
+    public float gravity = 10.0f;
+    public float groundDrag = 20.0f;
+    public float jumpForce = 6.0f;
+    public float airAccelerationSpeed;
+    private Vector3 moveDirection;
+    private Vector3 currentVelocity;
+    private float horizontalAxis, verticalAxis;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Setting the cursors lock state.
+        Cursor.lockState = CursorLockMode.Locked;
+
+        // Get the character controller in the player.
+        controller = playerObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Checking if we want to unlock the mouse.
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (Cursor.lockState == CursorLockMode.Locked)
+                Cursor.lockState = CursorLockMode.None;
+            else
+                Cursor.lockState = CursorLockMode.Locked;
+        }
+    }
+
+    private void UpdateMoveVector()
+    {
+        horizontalAxis = Input.GetAxisRaw("Horizontal");
+        verticalAxis = Input.GetAxisRaw("Vertical");
+        moveDirection = (horizontalAxis * playerObject.transform.right) + (verticalAxis * playerObject.transform.forward);
+        moveDirection.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        // Update the move vector.
+        this.UpdateMoveVector();
+
+        if (controller.isGrounded)
+        {
+            // Calculating movement.
+            currentVelocity = moveDirection * speed;
+            currentVelocity.y -= groundDrag;
+
+            if (Input.GetButton("Jump"))
+                currentVelocity = (playerObject.transform.forward + moveDirection * jumpForce) + Vector3.up * jumpForce;
+        }
+        else
+        {
+            // Calculating gravity.
+            Vector3 gravityVector = Vector3.down * gravity * Time.deltaTime;
+            currentVelocity += gravityVector;
+
+            Vector3 inAirMoveVector = moveDirection * speed;
+            inAirMoveVector -= currentVelocity;
+
+            Vector3 velocityDiff = Vector3.ProjectOnPlane(inAirMoveVector, gravityVector);
+
+            currentVelocity += velocityDiff * airAccelerationSpeed * Time.deltaTime;
+        }
+
+        controller.Move(currentVelocity * Time.deltaTime);
+    }
+
+}
