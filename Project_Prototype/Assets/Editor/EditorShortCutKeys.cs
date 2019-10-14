8153558bcc7e3e697d16c85d5fe6cfa758f4a4fc
Cioty <48895248+Cioty@users.noreply.github.com>
2019-10-15 00:30:05 +1100

    Monday night map commit. Includes editor scripts.

---
+++ Project_Prototype/Assets/Editor/EditorShortCutKeys.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class EditorShortCutKeys : ScriptableObject
+{
+    [MenuItem("GameObject/Deselect all %#D")]
+    static void DeselectAll()
+    {
+        Selection.objects = new Object[0];
+    }
+}
