ef62d803d76029f2cf193114236d4f9d1a61a10a
NixSane <50092494+NixSane@users.noreply.github.com>
2019-10-15 16:15:25 +1100

    Use the RespawnArray.cs script on any empty Game object to to contain the array of spawn points. Set the number of spawn points to anything you want and drag n drop any game object of your choosing to spawn the Player.

---
+++ Project_Prototype/Assets/Scripts/Mech_Recovery.cs
@@ -0,0 +1,103 @@
+﻿/*=============================================================================
+ * Game:        Metallicide
+ * Version:     Alpha
+ * 
+ * Class:       Mech_Recovery.cs
+ * Purpose:     Manages the respawing and recovery of the Mech player state.
+ * 
+ * Author:      Nixon Sok
+ * Team:        Skylighter
+ * 
+ * Deficiences:
+ * 
+ *===========================================================================*/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mech_Recovery : MonoBehaviour
+{
+    // The Player in Ball state
+    public GameObject inGameBall;
+    public GameObject inGameMech;
+    private GameObject ball;
+
+    // This will be used for onTriggerCollisions
+    private StateManager playerState;
+
+    // Respawn timer
+    public float deathTimer = 5;
+
+    // For the Ball/Player dies
+    private PlayerHandler ballHandler;
+    private PlayerHandler mechHandler;
+
+    // Character controller
+    CharacterController enabler;
+
+    // Death Screen
+    // public Canvas deathScreenEnabler;
+
+    private void Awake()
+    {
+        
+        playerState = inGameBall.GetComponentInParent<StateManager>();
+        ballHandler = inGameBall.GetComponent<PlayerHandler>();
+        mechHandler = inGameMech.GetComponent<PlayerHandler>();
+
+        enabler = inGameMech.GetComponentInParent<CharacterController>();
+    }
+
+    
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ball = collision.gameObject;
+
+        //if (ball == inGameBall)
+        //{
+        //    playerState.SetState(StateManager.PLAYER_STATE.Mech);
+        //}
+        //else if (ballHandler.Health <= 0)
+        //{
+        //    playerState.SetState(StateManager.PLAYER_STATE.Mech);
+        //    inGameMech.transform.position = transform.position;
+        //    ballHandler.Health = 10;
+        //}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // MechRespawn();
+    }
+
+    void MechRespawn()
+    {
+        // If Mech's health is below zero
+        if (mechHandler.mechHealth <= 0.0f)
+        {
+            mechHandler.mechHealth = 0;
+            enabler.enabled = false;
+            //deathScreenEnabler.enabled = true;
+        }
+
+        if (mechHandler.mechHealth == 0)
+        {
+            if (deathTimer <= 0)
+            {
+                inGameMech.transform.parent.position = transform.position;
+                enabler.enabled = true;
+                mechHandler.mechHealth = 2;
+                deathTimer = 5;
+            }
+            else if (deathTimer > 0)
+            {
+                deathTimer -= 1 * Time.deltaTime;
+                Debug.Log("Time til respawn " + deathTimer.ToString());
+            }
+        }
+    }
+}
---
+++ Project_Prototype/Assets/Scripts/RespawnArray.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnArray : MonoBehaviour
+{
+    public GameObject playerObject;
+
+
+
+    // The Player in Ball state
+    public GameObject inGameBall;
+    public GameObject inGameMech;
+    private GameObject ball;
+
+    // This will be used for onTriggerCollisions
+    private StateManager playerState;
+
+    // Respawn timer
+    public float deathTimer = 3;
+
+    // For the Ball/Player dies
+    private PlayerHandler ballHandler;
+    private PlayerHandler mechHandler;
+    private PlayerHandler playerHandler;
+
+    // Character controller
+    CharacterController enabler;
+
+    // Keeps track of all the spawn points
+    public List<Mech_Recovery> respawnPoints = new List<Mech_Recovery>();
+
+    // Random Number to pick a random spawn point
+    int randNumber;
+
+    private void Awake()
+    {
+
+        playerState = inGameBall.GetComponentInParent<StateManager>();
+        
+        playerHandler = playerObject.GetComponent<PlayerHandler>();
+        
+
+        enabler = inGameMech.GetComponentInParent<CharacterController>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Random.InitState((int)System.DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MechRespawn();
+    }
+
+    void MechRespawn()
+    {
+        // If Mech's health is below zero
+        //if (mechHandler.Health < 0.0f)
+        //{
+        //    mechHandler.Health = 0;
+        //    enabler.enabled = false;
+        //    //deathScreenEnabler.enabled = true;
+        //    // randNumber = Random.Range(0, respawnPoints.Count);
+        //}
+
+        if (playerHandler.mechHealth <= 0)
+        {
+            if (enabler.enabled)
+            {
+                enabler.enabled = false;
+            }
+
+            if(playerHandler.mechHealth < 0)
+                playerHandler.mechHealth = 0;
+            
+            randNumber = Random.Range(0, respawnPoints.Count);
+            if (deathTimer <= 0)
+            {  
+                inGameMech.transform.parent.position = respawnPoints[randNumber].transform.position;
+                enabler.enabled = true;
+                playerHandler.mechHealth = 2;
+                deathTimer = 3;
+            }
+            else if (deathTimer > 0)
+            {
+                deathTimer -= 1 * Time.deltaTime;
+                Debug.Log("Time til respawn " + deathTimer.ToString());
+            }
+        }
+    }
+}
