4a91266878b0e2e4782ea69a1a9c8489720ad107
NixSane <50092494+NixSane@users.noreply.github.com>
2019-11-10 12:10:55 +1100

    A working Ball_Movement script with acceleration. rigidBody drag must be turned on.

--- Project_Prototype/Assets/Scripts/Ball_Movement.cs
+++ Project_Prototype/Assets/Scripts/Ball_Movement.cs
@@ -1,30 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using XboxCtrlrInput;
 
 public class Ball_Movement : MonoBehaviour
 {
     // References
-    [Header("This object's children components")]
-    public GameObject coreObject;
-    public Rigidbody rigidBody;
-    public SphereCollider sphereCollider;
-
     public GameObject thirdPersonCamera;
+    public GameObject sphereObject;
+    public GameObject coreObject;
+    public GameObject coreCage;
+
+    // Getting the rigidbody and collider from the sphere object
+    private SphereCollider sphereCollider;
+    private Rigidbody rb;
     private PlayerHandler playerHandler;
 
     // Movement
-    [Header("Movement")]
-    public float movementSpeed = 5.0f;
     private float currentSpeed;
+    public float movementSpeed = 5.0f;
     public float maxSpeed = 10.0f;
+    private float acceleration = 0.0f;
 
-    // Creating acceleration
-    Vector3 currentVelocity = Vector3.zero;
-    Vector3 newVelocity = Vector3.zero;
-    Vector3 acceleration = Vector3.zero;
-    Vector3 movementDirection;
+    private float smooth = 1.0f;
+
+    Vector3 currentVelocity;
+    Vector3 direction;
+
+    // Cage rotation
+    Quaternion currentCageRotX;
+    Quaternion newCageRotX;
+
 
     // Jumping 
     public float jumpForce = 5.0f;
@@ -32,28 +37,25 @@ public class Ball_Movement : MonoBehaviour
 
     // Collider variables
     public float distanceToGround = 0.5f;
-  
-    private void Start()
+
+    private void Awake()
     {
+        rb = sphereObject.GetComponent<Rigidbody>();
+        sphereCollider = sphereObject.GetComponent<SphereCollider>();
         playerHandler = GetComponentInParent<PlayerHandler>();
-        sphereCollider = GetComponent<SphereCollider>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentCageRotX = new Quaternion(coreCage.transform.localRotation.x, 0.0f, 0.0f, 0.0f);
     }
 
     private void Update()
     {
-        if(playerHandler.HasAssignedController)
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
-            if (XCI.GetButton(XboxButton.A, playerHandler.AssignedController) && IsGrounded())
-            {
-                isJumping = true;
-            }
-        }
-        else
-        {
-            if (Input.GetButtonDown("Jump") && IsGrounded())
-            {
-                isJumping = true;
-            }
+            isJumping = true;
         }
     }
 
@@ -61,10 +63,11 @@ public class Ball_Movement : MonoBehaviour
     void FixedUpdate()
     {
         ball_roll();
+        coreDirection();
 
         if (isJumping)
         {
-            rigidBody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isJumping = false;
         }
     }
@@ -73,70 +76,76 @@ public class Ball_Movement : MonoBehaviour
     // Making the ball move in the direction the camera is facing
     void ball_roll()
     {
-        float horizontal_move = 0f;
-        float vertical_move = 0f;
+        float horizontal_move = Input.GetAxis("Horizontal");
+        float vertical_move = Input.GetAxis("Vertical");
 
-        if (playerHandler.HasAssignedController)
-        {
-            horizontal_move = XCI.GetAxis(XboxAxis.LeftStickX, playerHandler.AssignedController);
-            vertical_move = XCI.GetAxis(XboxAxis.LeftStickY, playerHandler.AssignedController);
-        }
-        else
-        {
-            horizontal_move = Input.GetAxis("Horizontal");
-            vertical_move = Input.GetAxis("Vertical");
-        }
-
+        // So the ball moves where the Camera faces
         Vector3 cam_forward = thirdPersonCamera.transform.forward;
         Vector3 cam_right = thirdPersonCamera.transform.right;
 
+        // Locking camera's y rotation to 0, so it doesn't tilt away from the forward axis.
         cam_forward.y = 0.0f;
         cam_right.y = 0.0f;
-        
-        // Getting the Ball's movement direction
-        movementDirection = (cam_forward * vertical_move + cam_right * horizontal_move);
-        movementDirection.Normalize();
+        cam_forward = cam_forward.normalized;
+        cam_right = cam_right.normalized;
 
-        // Check for player input
-        if (movementDirection.magnitude > 0)
+        // The direction
+        direction = (cam_forward * vertical_move + cam_right * horizontal_move);
+
+        // Check if there's input
+        if (direction.magnitude != 0.0f)
         {
-            if (currentSpeed < maxSpeed)
-                currentSpeed += (maxSpeed - movementSpeed);
-            else
+            // Acceleration achieved through dividing
+            currentSpeed = Mathf.Lerp(currentSpeed, maxSpeed, movementSpeed/maxSpeed);
+
+            if (currentSpeed > maxSpeed)
             {
                 currentSpeed = maxSpeed;
             }
-
-            acceleration = Vector3.Lerp(acceleration, movementDirection * maxSpeed, currentSpeed / maxSpeed);
         }
         else
-        {
-            if (currentSpeed > 0)
-                currentSpeed -= (maxSpeed - movementSpeed);
-            else
-                currentSpeed = 0.0f;
+        {           
+            // Deceleration achieved through dividing current speed by max speed
+            currentSpeed = Mathf.Lerp(currentSpeed, 0.0f, movementSpeed/maxSpeed);
 
-            acceleration = Vector3.Lerp(acceleration, Vector3.zero, currentSpeed / maxSpeed);
+            if (currentSpeed < 0.0f)
+            {
+                currentSpeed = 0.0f;
+            }
         }
 
-        currentVelocity = acceleration;
-        currentVelocity = Vector3.ClampMagnitude(acceleration, maxSpeed);
+        currentVelocity = direction * currentSpeed;
+        coreCage.transform.localRotation = coreCage.transform.localRotation * Quaternion.AngleAxis(coreCage.transform.localRotation.x + currentSpeed, Vector3.right);
 
-        rigidBody.AddForce(currentVelocity);
+        // Translate position
+        rb.AddForce(currentVelocity);
+        
+    }
+
+    void coreDirection()
+    {
+        // The core's inner rotation from Camera
+        coreObject.transform.localRotation = thirdPersonCamera.transform.localRotation;
     }
 
     private bool IsGrounded()
     {
         RaycastHit hit;
-        Ray ray = new Ray(coreObject.transform.position, -Vector3.up);
+        Ray ray = new Ray(transform.position, -Vector3.up);
 
         if (Physics.Raycast(ray, out hit))
         {
             GameObject targetHit = hit.transform.gameObject;
-            if (hit.distance <= distanceToGround)
+            if (targetHit && targetHit.tag == "Ground" && hit.distance <= distanceToGround)
+            {
+                Debug.Log("hit: " + hit.distance);
                 return true;
+            }
             else
+            {
+                Debug.Log("no: " + hit.distance);
                 return false;
+            }
         }
         else
             return false;
