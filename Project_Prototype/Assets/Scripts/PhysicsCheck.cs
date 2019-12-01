fb7283e12b26265d1b3d47df8f6668099d0ff7aa
Cioty <48895248+Cioty@users.noreply.github.com>
2019-12-02 00:54:22 +1100

    Collision and pipes.

---
+++ Project_Prototype/Assets/Scripts/PhysicsCheck.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsCheck : MonoBehaviour
+{
+    public GameObject physBall;
+    public List<GameObject> ballList;
+    private bool spawning = false;
+    public float spawnRate = 0.1f;
+    public int spawnAmount = 4;
+    private float spawnTimer;
+    public float spawnRange;
+    public GameObject ballParent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //create balls
+        if(Input.GetKeyDown(KeyCode.O) ) 
+        {
+            spawning = !spawning;
+            spawnTimer = spawnRate;
+        }
+
+        if(spawning)
+        {
+            if (spawnTimer <= 0)
+            {
+                for (int i = 0; i < spawnAmount; i++)
+                {
+                    BallCreate();
+                }
+                spawnTimer = spawnRate;
+            } else
+            {
+                spawnTimer -= Time.deltaTime;
+            }
+        }
+    }
+
+    private void BallCreate()
+    {
+        var spawnPosition = new Vector3(RangeCreator(transform.position.x, spawnRange), transform.position.y, RangeCreator(transform.position.z, spawnRange));
+        var ballObject = Instantiate(physBall, spawnPosition, Quaternion.identity, ballParent.transform);
+        ballList.Add(ballObject);
+
+        var randX = Random.Range(-0.5f, 0.5f);
+        var randZ = Random.Range(-0.5f, 0.5f);
+
+
+        ballObject.GetComponent<Rigidbody>().AddForce(new Vector3(randX, 0, randZ));
+    }
+
+    public float RangeCreator(float inital, float range)
+    {
+        return inital += Random.Range(-range, range);
+    }
+}
---
+++ Project_Prototype/Assets/Scripts/PhysicsCheckBall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsCheckBall : MonoBehaviour
+{
+    public float deathHeight = -30;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.y <=  deathHeight)
+        {
+            gameObject.name = "Leak";
+            GetComponent<Rigidbody>().isKinematic = true;
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+
+        //delete balls
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            Debug.Log("Cleared");
+            Destroy(gameObject);
+        }
+        //freezes balls
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            GetComponent<Rigidbody>().isKinematic = true;
+            Debug.Log("Frozen");
+        }
+    }
+}
